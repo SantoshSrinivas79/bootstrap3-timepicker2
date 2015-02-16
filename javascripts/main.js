e3037a331092449268e2c0dfe885a2b53e8faeca
acpmasquerade <d@picovico.com>
2015-02-16 20:26:24 +0545

    homepage with demos

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
-console.log('This would be the main JS file.');
+$(function(){
+	$('#timepicker-default').timepicker();
+	  $('#timepicker-24hr').timepicker({
+    minuteStep: 1,
+    template: 'modal',
+   appendWidgetTo: 'body',
+    showSeconds: true,
+    showMeridian: false,
+    defaultTime: false,
+    modalBackdrop: true
+  });
+});
